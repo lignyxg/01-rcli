047d3b6135464a4385671daa19b937f9275f3390
Gama <guannan@Gama.local>
2024-04-20 17:28:15 +0800

    support generic scv to json and yaml

--- src/opts.rs
+++ src/opts.rs
@@ -1,5 +1,8 @@
+use anyhow::anyhow;
 use clap::Parser;
+use std::fmt::{Display, Formatter};
 use std::path::Path;
+use std::str::FromStr;
 
 #[derive(Debug, Parser)]
 #[command(name = "rcli", version, author, about, long_about = None)]
@@ -14,17 +17,25 @@ pub enum SubCommand {
     Csv(CsvOpts),
 }
 
+#[derive(Debug, Copy, Clone)]
+pub enum OutputFormat {
+    Json,
+    Yaml,
+}
+
 #[derive(Debug, Parser)]
 pub struct CsvOpts {
     #[arg(short, long, value_parser = verify_input_file)]
     pub input: String,
-    /**
+    /*
     default_value 表示这个值需要转换,
     比如"output.json"是一个 &str, 它实现了From<T> trait,
     可以转换为 String, "output.json".into()
      */
-    #[arg(short, long, default_value = "output.json")]
-    pub output: String,
+    #[arg(short, long)]
+    pub output: Option<String>,
+    #[arg(long, value_parser = parse_format, default_value = "json")]
+    pub format: OutputFormat,
     // default_value_t 表示就是这个字面值, 不需要转换处理
     #[arg(short, long, default_value_t = ',')]
     pub delimiter: char,
@@ -37,5 +48,36 @@ fn verify_input_file(file_name: &str) -> Result<String, &'static str> {
         Ok(file_name.into())
     } else {
         Err("File does not exists")
+    }
+}
+
+fn parse_format(format: &str) -> Result<OutputFormat, anyhow::Error> {
+    format.parse()
+}
+
+impl FromStr for OutputFormat {
+    type Err = anyhow::Error;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s {
+            "json" => Ok(OutputFormat::Json),
+            "yaml" => Ok(OutputFormat::Yaml),
+            _ => Err(anyhow!("Invalid format")),
+        }
+    }
+}
+
+impl From<OutputFormat> for &'static str {
+    fn from(value: OutputFormat) -> Self {
+        match value {
+            OutputFormat::Json => "json",
+            OutputFormat::Yaml => "yaml",
+        }
+    }
+}
+
+impl Display for OutputFormat {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", Into::<&str>::into(*self))
     }
 }
--- src/process.rs
+++ src/process.rs
@@ -1,6 +1,8 @@
+use crate::opts::OutputFormat;
 use anyhow::Result;
 use csv::Reader;
 use serde::{Deserialize, Serialize};
+use serde_json::Value;
 use std::fs;
 
 #[derive(Debug, Deserialize, Serialize)]
@@ -15,14 +17,24 @@ pub struct Player {
     kit: u8,
 }
 
-pub fn process_csv(input: &str, output: &str) -> Result<()> {
+pub fn process_csv(input: &str, output: String, format: OutputFormat) -> Result<()> {
     let mut reader = Reader::from_path(input)?;
     let mut ret = Vec::with_capacity(128);
-    for result in reader.deserialize() {
-        let record: Player = result?;
-        ret.push(record);
+    let headers = reader.headers()?.clone();
+    for result in reader.records() {
+        let record = result?;
+        // headers.iter() 使用 headers 的迭代器
+        // record.iter() 使用 record 的迭代器
+        // zip() 将两个迭代器合并为一个元组的迭代器 [(header, record), ...]
+        // collect::<Value>() 将元组迭代器转换为 Json Value
+        // 因为 Json Value 也实现了迭代器 std::iter::Iterator trait
+        let json_value = headers.iter().zip(record.iter()).collect::<Value>();
+        ret.push(json_value);
     }
-    let json = serde_json::to_string_pretty(&ret)?;
-    fs::write(output, json)?;
+    let content = match format {
+        OutputFormat::Json => serde_json::to_string_pretty(&ret)?,
+        OutputFormat::Yaml => serde_yaml::to_string(&ret)?,
+    };
+    fs::write(output, content)?;
     Ok(())
 }
